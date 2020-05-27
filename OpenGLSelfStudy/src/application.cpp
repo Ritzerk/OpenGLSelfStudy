f1fd28010e69112460d40f6640cfe5778a84fe7f
Wiktoria Kazmierczak <kazmierw@uni.coventry.ac.uk>
2020-05-27 15:20:55 +0100

    Add project files.

---
+++ OpenGLSelfStudy/src/application.cpp
@@ -0,0 +1,33 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Tab name", NULL, NULL);
+	if (window == NULL) 
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1; 
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	return 0; 
+
+
+}
