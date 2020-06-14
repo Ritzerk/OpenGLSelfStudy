624948a0b6b085dc70ade4086317bd1e798cd1fd
Ritzerk <ritzerk@gmail.com>
2020-06-14 10:45:57 +0100

    Textures stuck on loading image

--- OpenGLSelfStudy/shaders/shader.fs
+++ OpenGLSelfStudy/shaders/shader.fs
@@ -1,8 +1,14 @@
 #version 330 core
 out vec4 FragColour;
-in vec4 vertexColour;
+
+in vec3 ourColour;
+in vec2 TxtCoord;
+
+uniform sampler2D texture1;	//built in sampler for what we need
+uniform sampler2D texture2;
 
 void main()
 {
-FragColour = vertexColour;
+	//FragColour = texture(ourTexture, TxtCoord) * vec4(ourColour, 1.0); //texture function links texture coordinates to the sampler
+	FragColour = mix(texture(texture1, TxtCoord), texture(texture2, TxtCoord), 0.2);
 }
--- OpenGLSelfStudy/src/application.cpp
+++ OpenGLSelfStudy/src/application.cpp
@@ -1,7 +1,11 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <iostream>
+#include <filesystem>
 #include "Shader.h"
 
 void adjustViewportToWindowSize(GLFWwindow* window, int width, int height);
@@ -32,32 +36,108 @@ int main(void)
 	glViewport(0, 0, 800, 600);	//size of GL rendering window. 
 	glfwSetFramebufferSizeCallback(window, adjustViewportToWindowSize);		//Viewport needs to be resized everytime user resizes window, this is done using a callback. 
 
+	
+
+	
+
 	Shader firstShader("shaders/shader.vs", "shaders/shader.fs");
 	
 
 	float triangleVertices[] = {
-		//positions vec 4		//colours rgb vec 3
-		-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,		
-		 0.5f, -0.5f, 0.0f,	    0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f
+		//positions vec 4		//colours rgb vec 3		//texture coords
+		 0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,		1.0f, 1.0f,		//top right
+		 0.5f, -0.5f, 0.0f,	    0.0f, 1.0f, 0.0f,		1.0f, 0.0f,		//bottom right
+		-0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,		0.0f, 0.0f,		//bottom left
+		-0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f,		0.0f, 1.0f		//top left
 	}; 
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
+	};
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	//Bind Buffers to data next
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	//Do the same for colour
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	//Do the same for texture
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	
+	
+
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	//Set the texture wrapping/filtering options on the currently bound texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);		//Repeats texture where the object is bigger than texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	//Linear for downscailing images - the returned colour of pixel is a mix (interpolated) value of colours, if we are downscailing, then we can't show every single pixel on the image inside the object.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   //Linear as we don't use mipmap when upscailing. 
+
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* txtjpg = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	std::cout << stbi_failure_reason() << std::endl;
+
+	if (txtjpg)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, txtjpg);	//The bound object now has the texture bound to it.
+		glGenerateMipmap(GL_TEXTURE_2D);		//A mipmap is a set of different sizes of the texture, to best suit the dimensions of our shape we link the texture too. This function auto-generates those mipmaps.
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(txtjpg);	//Texture is already bound, so we can free the image memory.
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	//Set the texture wrapping/filtering options on the currently bound texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);		//Repeats texture where the object is bigger than texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	//Linear for downscailing images - the returned colour of pixel is a mix (interpolated) value of colours, if we are downscailing, then we can't show every single pixel on the image inside the object.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   //Linear as we don't use mipmap when upscailing. 
+
+
+	unsigned char* data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else 
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	
+
+	firstShader.use();
+	glUniform1i(glGetUniformLocation(firstShader.programID, "texture1"), 0);
+	firstShader.setInt("texture2", 1);
 
 	glBindVertexArray(0);
 	
@@ -72,12 +152,14 @@ int main(void)
 		//glUseProgram(shaderProgram);				//Every rendering call will have to use this program, hence use the shaders. If no VAO is bound, it has to be bound now.
 		
 		firstShader.use();
-		float offset = 0.5f;
-		firstShader.setFloat("xOffset", offset);
 
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3); 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); 
 
 		glBindVertexArray(0);
 
