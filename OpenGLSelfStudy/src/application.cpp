92bfd72069f18f9122cbf568494279d02fc1b4ef
Ritzerk <ritzerk@gmail.com>
2020-06-21 16:55:57 +0100

    Camera System: Moving with the mouse, no mouse class yet.

--- OpenGLSelfStudy/src/application.cpp
+++ OpenGLSelfStudy/src/application.cpp
@@ -14,7 +14,26 @@
 
 void adjustViewportToWindowSize(GLFWwindow* window, int width, int height);
 void checkEsc(GLFWwindow* window);
-void processInput(GLFWwindow* window, float &val);
+void processInput(GLFWwindow* window, float& val);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+
+//camera position
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 int main(void)
 {
@@ -38,10 +57,14 @@ int main(void)
 		return -1; 
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	glViewport(0, 0, 800, 600);	//size of GL rendering window. 
 	glfwSetFramebufferSizeCallback(window, adjustViewportToWindowSize);		//Viewport needs to be resized everytime user resizes window, this is done using a callback. 
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
-	glEnable(GL_DEPTH_TEST);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	Shader firstShader("shaders/shader.vs", "shaders/shader.fs");
 
@@ -172,22 +195,26 @@ int main(void)
 
 	stbi_image_free(data);
 
-	
-
 	firstShader.use();
 	firstShader.setInt("texture1", 0); //glUniform1i(glGetUniformLocation(firstShader.programID, "texture1"), 0);
 	firstShader.setInt("texture2", 1);
 
-
 	float visibility = 1.0f;
 
 	glm::mat4 identityMatrix = glm::mat4(1.0f); //this creates an identity matrix
-	
+
+	glm::mat4 projection = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
-		checkEsc(window);	
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		processInput(window, visibility);
+		glfwSetScrollCallback(window, scroll_callback);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    //calling glClear sets the background to color values set by glClearColor function.
@@ -199,13 +226,9 @@ int main(void)
 
 		firstShader.use();
 
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		//create transformations for how we see the objects
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+		projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		
-
 		firstShader.setMatrix4fv("projection", glm::value_ptr(projection));
 		firstShader.setMatrix4fv("view", glm::value_ptr(view));
 
@@ -257,6 +280,18 @@ void checkEsc(GLFWwindow *window)
 
 void processInput(GLFWwindow *window, float &val) 
 {
+	checkEsc(window);
+
+	const float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) 
 	{
 		val += 0.001f;
@@ -271,4 +306,46 @@ void processInput(GLFWwindow *window, float &val)
 			val = 0.0f;
 		}
 	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	//calculate the offset movement between the last and current frame
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov< 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
 }
